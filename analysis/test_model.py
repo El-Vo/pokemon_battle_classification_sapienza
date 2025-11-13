60b679fdc439cb872c30ac161f4e7410a0d3a118
El-Vo <vollrathe@t-online.de>
2025-11-13 23:16:11 +0100

    make submission filepaths configurable for kaggle environment

--- analysis/test_model.py
+++ analysis/test_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from datetime import datetime
 import os
 
+
 class TestModel:
 
     def __init__(self, model: any, test_df: pd.DataFrame, features):
@@ -9,21 +10,20 @@ class TestModel:
         self.test_df = test_df
         self.test_features = self.test_df[features]
 
-    def test(self):
+    def test(self, submission_filepath: str):
         # Make predictions on the test data
         print("Generating predictions on the test set...")
         test_predictions = self.model.predict(self.test_features)
 
         # Create the submission DataFrame
-        submission_df = pd.DataFrame({
-            'battle_id': self.test_df['battle_id'],
-            'player_won': test_predictions
-        })
+        submission_df = pd.DataFrame(
+            {"battle_id": self.test_df["battle_id"], "player_won": test_predictions}
+        )
 
         # Ensure results directory exists and save the DataFrame to a timestamped .csv file
-        os.makedirs('results', exist_ok=True)
+        os.makedirs("results", exist_ok=True)
         now = datetime.now()
-        filename = f"results/submission_{now:%Y%m%d_%H%M%S}.csv"
+        filename = f"{submission_filepath}/submission_{now:%Y%m%d_%H%M%S}.csv"
         submission_df.to_csv(filename, index=False)
 
         print(f"\nSubmission file created: {filename}")
--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def test_trained_model(model: Any, features: list, test_data_filepath: str) -> N
     )
     test_df = test_data_extractor.process()
     test = TestModel(model, test_df, features)
-    test.test()
+    test.test("results")
 
 
 if __name__ == "__main__":
